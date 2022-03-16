51b54c22cf26d8767618a75d76af885fd5bf939e
Robert Albenda <ralbenda@gmail.com>
2022-03-16 17:05:00 -0400

    set up some basic react front end

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import axios from "axios";
 
-render(<hr />, document.querySelector('#root'));
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      models: [],
+      brands: [],
+    };
+  }
+
+  async componentDidMount() {
+    const models = await axios.get("/api/models");
+    const modelsResponse = models.data;
+    this.setState({
+      models: modelsResponse,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome To Robby's Store!</h1>
+        <h1>Models:</h1>
+        <ul>
+          {this.state.models.map((model) => {
+            return (
+              <li key={model.id}>
+                {model.brand.name} {model.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+render(<App />, document.querySelector("#root"));
